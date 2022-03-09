395ef63a7bc53a8b0e530db0f2db670502bdf7c9
goksu1 <goksu.baykal@gmail.com>
2022-03-09 10:01:59 +0300

    Done.

--- src/__test__/QuoteForm.test.js
+++ src/__test__/QuoteForm.test.js
@@ -5,6 +5,7 @@ import { Provider } from "react-redux";
 import store from "../store";
 import QuoteForm from "../features/quotes/QuoteForm";
 
+
 beforeEach(() => {
   render(
     <Provider store={store}>
--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store";
-
+import { v4 as uuid } from "uuid";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
+const id = uuid();
+console.log(id); // '1fd3234' 
+
+
 
 ReactDOM.render(
   <Provider store={store}>
